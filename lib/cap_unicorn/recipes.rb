f562913564c7afcaf5436cbe64e4b675dbf489dc
Scott Taylor <scott@railsnewbie.com>
2010-06-29 17:32:14 -0400

    first commit

---
+++ lib/cap_unicorn/recipes.rb
@@ -0,0 +1,43 @@
+namespace :unicorn do
+  def self.master_pid
+    pid = capture "pid=$(ps aux | grep unicorn_rails | grep master | grep -v grep); echo $pid"
+
+    unless pid.empty?
+      pid.split(" ")[1]
+    end
+  end
+
+  desc "Start unicorn"
+  task :start do
+    run "unicorn_rails -D -E #{rails_env} -c #{current_path}/config/unicorn/#{rails_env}.rb"
+  end
+
+  task :restart do
+    if pid = master_pid
+      run "kill -s USR2 #{pid}"
+    end
+  end
+
+  task :stop do
+    if pid = master_pid
+      run "kill -s TERM #{pid}"
+    end
+  end
+end
+
+namespace :deploy do
+  desc "Start the unicorn workers"
+  task :start do
+    unicorn.start
+  end
+
+  desc "Stop the unicorn workers"
+  task :stop do
+    unicorn.stop
+  end
+
+  desc "Restart the unicorn workers"
+  task :restart do
+    unicorn.restart
+  end
+end
